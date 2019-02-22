3272026d3c332138912fd3c90c2fa259d1473910
CHETAN <CHETAN@CHETAN-PC>
2019-02-22 13:41:13 +0530

    Changes done

---
+++ NaukariFrameWork/src/test/java/com/qa/tests/homepage.java
@@ -0,0 +1,21 @@
+package com.qa.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class homepage {
+	
+	public WebDriver driver;
+	
+	@Test
+	public void getBrowser()
+	{
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		driver= new ChromeDriver();
+		driver.get("https://google.com");
+		driver.manage().window().maximize();
+	}
+
+}
